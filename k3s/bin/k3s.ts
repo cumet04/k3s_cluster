968b3ca01f6aa57168280af970283485c62e3bf1
medalhkr <medalhkr@gmail.com>
2019-09-16 16:45:47 +0900

    cdk init
    use yarn & omit test

---
+++ k3s/bin/k3s.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { K3SStack } from '../lib/k3s-stack';
+
+const app = new cdk.App();
+new K3SStack(app, 'K3SStack');
