58d9084a7243a1bc2ab3243ae70ef29ada731495
cumet04 <medalhkr@gmail.com>
2020-04-23 01:19:38 +0900

    prettier 2

--- k3s/bin/k3s.ts
+++ k3s/bin/k3s.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { K3SStack } from '../lib/k3s-stack';
+import "source-map-support/register";
+import cdk = require("@aws-cdk/core");
+import { K3SStack } from "../lib/k3s-stack";
 
 const app = new cdk.App();
-new K3SStack(app, 'K3SStack');
+new K3SStack(app, "K3SStack");
